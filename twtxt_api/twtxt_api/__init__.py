4bec4a51639d70c9814fcb7eea03677aae24e8a6
Gil Gonçalves <lursty@gmail.com>
2020-04-10 19:16:47 +0100

    Add super naive endpoint for getting a user's twtxt
    Still just a blob of text, needs more parsing, but it does work.

--- twtxt_api/twtxt_api/__init__.py
+++ twtxt_api/twtxt_api/__init__.py
@@ -1,6 +1,8 @@
 __version__ = '0.1.0'
 
 from flask import Flask
+import requests
+
 app = Flask(__name__)
 
 REGISTRY = [
@@ -14,3 +16,41 @@ def registry():
 
     """
     return {"registry": REGISTRY}
+
+@app.route('/user/<string:username>')
+def user(username):
+    """
+    Get a user's twtxts
+
+    """
+    user_url = find_user_url(username)
+
+    user_twtxts = get_twtxts(user_url)
+
+    return {"user": username, "twtxts": user_twtxts, "url": user_url}
+
+
+def find_user_url(username):
+    for registry in REGISTRY:
+        url = get_user_url_from_registry(registry, username)
+        if url:
+            return url
+
+def get_user_url_from_registry(registry_url, username):
+    """
+    A registry URL is a URL to a file with lines containing a username and a url separated by a space
+
+    """
+    users_list = requests.get(registry_url)
+    for line in users_list.content.decode().split("\n"):
+        username, url = line.split(" ")
+        if username == username:
+            return url
+
+def get_twtxts(user_url):
+    """
+    A user url contains a list of twtxts
+
+    """
+    request = requests.get(user_url)
+    return request.content.decode()
