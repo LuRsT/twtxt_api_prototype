418e1c433b61849317dbca5a4c7f380c1f341e4c
Gil Gonçalves <lursty@gmail.com>
2020-04-10 18:54:27 +0100

    Add Registry endpoint

--- twtxt_api/twtxt_api/__init__.py
+++ twtxt_api/twtxt_api/__init__.py
@@ -1,3 +1,16 @@
 __version__ = '0.1.0'
 
+from flask import Flask
+app = Flask(__name__)
 
+REGISTRY = [
+    "https://raw.githubusercontent.com/mdom/we-are-twtxt/master/we-are-twtxt.txt",
+]
+
+@app.route('/registry')
+def registry():
+    """
+    List of registries in this API, hardcoded
+
+    """
+    return {"registry": REGISTRY}
